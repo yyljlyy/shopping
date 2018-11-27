352bb7be87d563fa2a0cc675fce334b7d2f7e35e
jian.li02 <jian.li02@luckincoffee.com>
2018-11-27 18:31:03 +0800

    fix bug for html

--- shopping-backend/src/main/java/com/luckin/innovation/group/entity/Product.java
+++ shopping-backend/src/main/java/com/luckin/innovation/group/entity/Product.java
@@ -15,7 +15,7 @@ import java.util.Date;
 public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
     /**
      * 商品标题
      */
@@ -56,11 +56,11 @@ public class Product implements Serializable {
     @Transient
     private ProductCategory categorySec;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
